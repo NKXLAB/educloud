0476de2610d393a3d056a5f48942cb1b118ae4b4
diegotremper <diegotremper@01b0759c-8364-8d5f-b991-e4a844365b43>
2011-03-12 21:08:40 +0000

    added build script to all projects
    Fixes issue 3

---
+++ cloudnode/src/com/google/educloud/cloudnode/App.java
@@ -0,0 +1,13 @@
+package com.google.educloud.cloudnode;
+
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
